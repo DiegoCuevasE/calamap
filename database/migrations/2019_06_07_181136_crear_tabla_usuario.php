aef4fab7790ae4391d124a9b4055ab69e2825ade
Franco <f.carvajalm@hotmail.com>
2019-06-07 18:47:46 -0400

    Usuario actualizado

--- database/migrations/2019_06_07_181136_crear_tabla_usuario.php
+++ database/migrations/2019_06_07_181136_crear_tabla_usuario.php
@@ -14,7 +14,20 @@ class CrearTablaUsuario extends Migration
     public function up()
     {
         Schema::create('usuario', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('cod_usuario');
+            $table->string('nombre_usuario', 50);
+            $table->string('apeliido_usuario', 50);
+            $table->string('telefono_usuario', 15);
+            $table->string('celular_usuario',15);
+            $table->string('contraseña_usuario', 100);
+            $table->dateTime('fecha_ingreso_usuario');
+            $table->dateTime('fecha_salida_usuario');
+            $table->string('nacionalidad_usuario', 15);
+            $table->string('genero_usuario', 15);
+            $table->date('fecha_nacimiento_usuario');
+            $table->string('correo_usuario', 50);
+            $table->string('tipo_usuario',20);
+            $table->boolean('estado_usuario');
             $table->timestamps();
         });
     }
